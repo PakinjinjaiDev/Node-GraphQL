25f4a03443d530b2e0ce1093f29252bf4807f9b0
Mr.Pakinjinjai <pakin.developer@gmail.com>
2025-02-14 16:07:06 +0700

    Update schema combined

--- graphql/schema.js
+++ graphql/schema.js
@@ -25,6 +25,47 @@ const schema = buildSchema(`
    customer_id: String
    customer_name: String
    sale_id: Float
+   sale_name: String
+   sale_team: String
+   service: String
+   so_type_2: String
+   type_cus: String
+   enuser: Float
+   prorate: Float
+   rev_total: Float
+   vmi: Float
+   rev_cloud: Float
+   avg_vmi:Float
+   total_actual: Float
+   total_actual_cost: Float
+   total_eng_cost: Float
+   sale_factors: Float
+   total_internal: Float
+   total_external_jv: Int
+   total_external: Float
+   int_inet: Float
+   ext_jv: Float
+   external: Float
+   total_cpu: Int
+   total_disk: Int
+   total_memory: Int
+   int_cloud: Float
+   external_cloud: Int
+   int_colocation: Float
+   external_colocation: Int
+   int_jv: Float
+   external_jv: Int
+   int_lead: Float
+   external_lead: Int
+   int_ms: Float
+   external_ms: Int
+   int_network: Float
+   external_network: Int
+   int_security: Float
+   external_security: Int
+   int_trading: Float
+   external_trading: Int
+   update_at_etl: String
   }
 `);
 
