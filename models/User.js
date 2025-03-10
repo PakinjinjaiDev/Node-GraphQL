6d34d50f57828681c4dc54e03a79cce88113b88e
Mr.Pakinjinjai <pakin.developer@gmail.com>
2025-03-10 14:33:57 +0700

    Update User.js

--- models/User.js
+++ models/User.js
@@ -1,5 +1,4 @@
 const { DataTypes } = require("sequelize");
-const mssql = require("../config/mssql"); // เชื่อมต่อกับ MSSQL
 const mysql = require("../config/mysql"); // Connection MySQL
 
 const User = mysql.define("User", { 
