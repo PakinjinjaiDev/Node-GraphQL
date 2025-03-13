2c4bcf728af5c375785224457b7456422752fad3
Mr.Pakinjinjai <pakin.developer@gmail.com>
2025-03-13 13:48:53 +0700

    Add switch
    short message error response

--- routes/graphql.js
+++ routes/graphql.js
@@ -6,12 +6,19 @@ module.exports = createHandler({
   schema,
   rootValue: resolvers,
   formatError: (err) => {
-    // log error เพื่อ debug
-    // console.error("GraphQL Error:", err);
-    return {
-      status: "error",
-      message: "มีข้อผิดพลาดบางอย่างในการร้องขอ กรุณาตรวจสอบ Query หรือ Mutations ของคุณ",
-      // detail: `${err}`
-    };
-  },
+    const message = err.message;
+    switch (true) {
+      case message.includes("Cannot query field"):
+        return { message: "Cannot query field" };
+      case message.includes("Syntax Error") && message.includes("Unexpected character"):
+        return { message: "Unexpected character" };
+      case message.includes("Syntax Error"):
+        return { message: "Syntax Error" };
+      case message.includes("Unknown argument"):
+        return { message: "Unknown argument" };
+      default:
+        return { message };
+    }
+  }
 });
+
