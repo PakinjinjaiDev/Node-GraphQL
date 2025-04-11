5b0248241d5f82cfa935bfb36d1f67c30e8ab522
Mr.Pakinjinjai <pakin.developer@gmail.com>
2025-04-11 15:44:13 +0700

    Add helmet

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 require("dotenv").config();
 const express = require("express");
+const helmet = require("helmet");
 const cors = require("cors");
 const graphqlHandler = require("./routes/graphql");
 const basicAuth = require("./middleware/basicAuth"); // ✅ Import Middleware
 const logGraphQLRequest = require("./middleware/loggingDB");
 
 const app = express();
+app.use(helmet());
 app.use(cors());
 app.use(express.json());
 app.use(logGraphQLRequest);
